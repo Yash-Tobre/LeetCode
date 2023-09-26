19edcf964d8ed7366cb6e5102ba2faa5af34bafe
Yash Vishwanath Tobre <60502700+Yash-Tobre@users.noreply.github.com>
2023-09-26 15:24:55 -0500

    Update solution.py

--- 1431. Kids With Greatest Number of candies/solution.py
+++ 1431. Kids With Greatest Number of candies/solution.py
@@ -1,10 +1,25 @@
+#Approach 1
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int):
         max_candy = max(candies)
         results = []
         for i in range(len(candies)):
-            number = candies[i] + extraCandies
+            number = candies[i] + extraCandies #approach with unoptimized time complexity.
             if number>=max_candy:
+                results.append(True)
+            else:
+                results.append(False)
+
+        return results
+
+#Approach 2:
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int):
+        max_candy = max(candies)
+        results = []
+        for i in range(len(candies)):
+
+            if candies[i] + extraCandies>=max_candy: #approach with optimized time complexity.
                 results.append(True)
             else:
                 results.append(False)
